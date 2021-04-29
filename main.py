bb9941d95567f026c732c66a82c1c6bedf0cada3
minlaxz <minminlaxz@gmail.com>
2021-04-29 14:46:10 +0630

    Added time history.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tweepy
 from telethon import TelegramClient, events
-
+from datetime import datetime
 import twitter_secs as tt
 import telegram_secs as te
 
@@ -20,9 +20,12 @@ except:
 
 chat = 'Click&Tweet'
 client = TelegramClient('anon', te.api_id, te.api_hash)
+print("Waiting for new Telegram message event...")
+
 
 @client.on(events.NewMessage(chat))
 async def my_event_handler(event):
+    print("Waiting for new Telegram message event...")
     chath = await event.get_chat()
     senderh = await event.get_sender()
     chat_idh = event.chat_id
@@ -31,14 +34,14 @@ async def my_event_handler(event):
     # print(chath, senderh, chat_idh, sender_idh)
 
     def tweet(msg):
-        # Sample method, used to update a status, you can write message whatever you want to post in twitter
-        api.update_status(msg + " #TweetByLaxz" )
+        # Write message whatever you want to post in twitter
+        api.update_status(msg + " #LaxzAutoTweet" )
     
     if 'Click To Tweet' in event.raw_text:
         # print(event.raw_text)
-        print('Tweeted')
+        print(f'{datetime.now()}, Tweeted')
         tweet(event.raw_text)
-        await event.reply('auto reply to event "Click To Tweet", tweeted!')
+        # await event.reply('auto reply to event "Click To Tweet", tweeted!')
 
 client.start()
 client.run_until_disconnected()
