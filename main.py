51367bf9fedc474795d769ae6ac093ff7eebb700
minlaxz <minminlaxz@gmail.com>
2021-05-01 16:24:53 +0630

    Message needs to be a bit shorter Exception.

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from telethon import TelegramClient, events
 from datetime import datetime
 import twitter_secs as tt
 import telegram_secs as te
+from rich import pretty, print
 
+pretty.install()
 
 # Authenticate to Twitter
 auth = tweepy.OAuthHandler(tt.app_api_key, tt.app_api_secret)
@@ -18,39 +20,42 @@ try:
 except:
     print("Error during twitter authentication")
 
-chat = 'Click&Tweet'
-client = TelegramClient('anon', te.api_id, te.api_hash)
+chat = "Click&Tweet"
+client = TelegramClient("anon", te.api_id, te.api_hash)
 print("Waiting for new Telegram message event...")
 
 
 @client.on(events.NewMessage(chat))
 async def my_event_handler(event):
-    chath = await event.get_chat()
-    senderh = await event.get_sender()
-    chat_idh = event.chat_id
-    sender_idh = event.sender_id
 
     # print(chath, senderh, chat_idh, sender_idh)
 
     def tweet(msg):
         # Write message whatever you want to post in twitter
-        api.update_status(msg + " #LaxzAutoTweet" )
-    
-    if 'WhatsHappeningInMyanmar' in event.raw_text:
+        api.update_status(msg + " #LaxzTweeted")
+
+    if "WhatsHappeningInMyanmar" in event.raw_text:
         print(event.text)
-        print(f'{datetime.now()}, Tweeted')
-        tweet(event.text)
+        try:
+            tweet(event.text)
+            print(f"{datetime.now()}, Tweeted.")
+        except tweepy.error.TweepError:
+            chat_event = await event.get_chat()
+            sender_event = await event.get_sender()
+            chat_id = event.chat_id
+            sender_id = event.sender_id
+            print(
+                [
+                    f"Chat : {chat_event}",
+                    f"ChatID : {chat_id}",
+                    f"Sender : {sender_event}",
+                    f"SenderID : {sender_id}",
+                ]
+            )
+            print(event.text)
+            print(f"{datetime.now()}, Cannot Tweet!")
         # await event.reply('auto reply to event "Click To Tweet", tweeted!')
+
 
 client.start()
 client.run_until_disconnected()
-
-
-
-
-
-
-
-
-
-
