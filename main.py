9c7ac86fb756eeeae2f73b008a2b9497aad0cf85
minlaxz <minminlaxz@gmail.com>
2021-12-07 14:32:05 +0630

    debug: Debugging
    Sending stickers

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tweepy
 from telethon import TelegramClient, events
+from telethon.tl.functions.messages import GetStickerSetRequest
+from telethon.tl.types import InputStickerSetID
 import datetime
 from rich import pretty, print
 from rich.console import Console
@@ -15,11 +17,14 @@ auth = tweepy.OAuthHandler(twitter_app_api_key, twitter_app_api_secret)
 auth.set_access_token(twitter_access_token, twitter_access_token_secret)
 api = tweepy.API(auth)  # Create the actual interface to twitter, using credentials
 
+
 def getDateTime() -> str:
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+
 def printMessage(message: str, error: bool) -> None:
     print(f"[red] {getDateTime()} :\n {message}[/red]") if error else print(f"[green] {getDateTime()} :\n {message}[/green]")
+
 
 try:
     api.verify_credentials()
@@ -29,11 +34,22 @@ except:
 
 
 # Specify Channel name to caught broadcasting posts
-chat = "Click&Tweet" 
+chat = "Click&Tweet"
 # Authenticate to Telegram
 client = TelegramClient("anon", telegram_api_id, telegram_api_hash)
 # print("Hello", style="#af00ff")
 console.print("Waiting for new Telegram message event...", style=css.dodgerblue)
+
+
+async def sendMessageToTelegram(message: str, event_id: int) -> None:
+    await client.send_message(chat, message, comment_to=event_id)
+
+async def sendStickerToTelegram(event_id: int) -> None:
+    stickers = await client(GetStickerSetRequest(
+    stickerset=InputStickerSetID(
+        id=891885078961979401, access_hash=-6312850271542209510
+    )))
+    await client.send_file(chat, stickers.documents[10], comment_to=event_id)
 
 
 @client.on(events.NewMessage(chat))
@@ -42,51 +58,45 @@ async def my_event_handler(event: events.NewMessage.Event) -> None:
         # Post to twitter
         api.update_status(msg + "\n#LaxzTweeted")
 
-    if (
-        "WhatsHappeningInMyanmar" in event.raw_text
-        or "ASIEANengageNUG" in event.raw_text
-    ):
-        printMessage(message=f"-----------------------------------\n {event.text} \n-----------------", error=False)
+    if "WhatsHappeningInMyanmar" in event.raw_text or "ASIEANengageNUG" in event.raw_text:
+        printMessage(
+            message=f"-----------------------------------\n {event.text} \n-----------------",
+            error=False,
+        )
         try:
-            make_tweet(msg = event.text)
-            printMessage(message = "Tweeted", error = False)
-            await client.send_message(
-                chat,
-                f"Previous Event- **{event.id}** is Tweeted.",
-                comment_to=event.id + 1,
+            make_tweet(msg=event.text)
+            printMessage(message="Tweeted", error=False)
+            await sendMessageToTelegram(
+                message=f"Previous Event- **{event.id}** is Tweeted.",
+                event_id=event.id + 1,
             )
-            printMessage(message = f"Commented to Message ID : {event.id}", error = False)
+            printMessage(message=f"Commented to Message ID : {event.id}", error=False)
         except tweepy.TweepyException as e:
-            printMessage(message = f"[red]This need to be tweeted manually.[/red]", error = True)
+            printMessage(message=f"[red]This need to be tweeted manually.[/red]", error=True)
             if isinstance(e, list) and "code" in e[0] and e[0]["code"] == 187:
                 await client.send_message(
                     chat,
                     f"{getDateTime()} => **Error on event** => {event.id} - ```errorCode:{e[0]['code']}```",
                     comment_to=event.id + 1,
                 )
-                printMessage(message = f"Commented to Message ID : {event.id}", error = True)
+                printMessage(message=f"Commented to Message ID : {event.id}", error=True)
                 print(f"Error message : {e[0].message}")
             else:
                 print(f"[bold magenta] Unhandled Exception : {e} [/bold magenta]")
         print("-----------------------------------")
     elif "Goodnight" in event.raw_text:
-        print("\a") 
-        # x = re.findall("Goodnight", event.raw_text)
+        print("\a")
+        await sendMessageToTelegram(message="Client is disconnecting ...", event_id=event.id)
         if "Thomas" in event.raw_text:
-            await client.send_message(
-                chat,
-                "__Goodnight **Admin Thomas**__.`Be Safe`.",
-                comment_to=event.id,
-            )
+            await sendMessageToTelegram(message="__Goodnight **Admin Thomas**__.`Be Safe`.", event_id=event.id)
         else:
-            await client.send_message(
-                chat,
-                "__Goodnight **Admins**__. `Be Safe`.",
-                comment_to=event.id,
-            )
-        # await client.send_file('me', '/home/me/Pictures/holidays.jpg')
-        printMessage(message = f"[orange]{getDateTime()}, Script is gonna stop.[/orange]", error = False)
-        exit()
+            await sendMessageToTelegram(message="__Goodnight **Admin 🐼🎋**__. `Be Safe`.", event_id=event.id)
+        await sendStickerToTelegram(event_id=event.id)
+        printMessage(
+            message=f"[orange]{getDateTime()}, Script is gonna stop.[/orange]",
+            error=False,
+        )
+        client.disconnect()
     else:
         print("passed")
 
